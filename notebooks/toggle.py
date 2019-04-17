a1d959bf120669fa78797437fbce0cfa695bf6c6
Ming Zhao <35346970+ming-zhao@users.noreply.github.com>
2019-04-16 20:09:40 -0500

    Add files via upload

--- notebooks/toggle.py
+++ notebooks/toggle.py
@@ -2,28 +2,28 @@ import IPython.core.display as di
 from IPython.display import HTML
 
 di.display_html('''<script>
-	function code_toggle(e) {
-		while (!e.closest(".nboutput").previousElementSibling.classList.contains("nbinput")) {
-			e = e.closest(".nboutput").previousElementSibling;
-		}
-		var target = e.closest(".nboutput").previousElementSibling;
-        if (target.getAttribute("style") == "" || target.getAttribute("style") == null) {
-            target.style.display = "none";
-        }
-        else {
-            target.style.removeProperty("display");
-        }
-    }
-	$(document).ready(function() {
-		var code_blocks = document.getElementsByClassName('nbinput docutils container');
-		for (var i = 0, len = code_blocks.length; i < len; ++i) {
-			if (code_blocks[i].innerHTML.indexOf("toggle") !== -1) {
-				code_blocks[i].style.display = "none";
-			}
-		}
-		//$('div.nbinput').hide();
-	});
-    $(document).ready(code_toggle);
+function code_toggle(e) {
+while (!e.closest(".nboutput").previousElementSibling.classList.contains("nbinput")) {
+e = e.closest(".nboutput").previousElementSibling;
+}
+var target = e.closest(".nboutput").previousElementSibling;
+if (target.getAttribute("style") == "" || target.getAttribute("style") == null) {
+target.style.display = "none";
+}
+else {
+target.style.removeProperty("display");
+}
+}
+$(document).ready(function() {
+var code_blocks = document.getElementsByClassName('nbinput docutils container');
+for (var i = 0, len = code_blocks.length; i < len; ++i) {
+if (code_blocks[i].innerHTML.indexOf("toggle") !== -1) {
+code_blocks[i].style.display = "none";
+}
+}
+//$('div.nbinput').hide();
+});
+$(document).ready(code_toggle);
 </script>''', raw=True)
 
 def toggle():
