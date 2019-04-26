316550bff4556841c77ccfbbdaf832620e9177a4
Ming Zhao <35346970+ming-zhao@users.noreply.github.com>
2019-04-26 09:50:10 -0500

    Add files via upload

---
+++ notebooks/init_Introduction_to_Statistics.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from ipywidgets import *
+%matplotlib inline
+sns.set(color_codes=True)
+
+import warnings
+warnings.filterwarnings('ignore')
